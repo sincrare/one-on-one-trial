2fe0f256ae16b715f0c606d5fe2559db1c0fa4b4
門田啓吾 <sincrare@sonicgarden.jp>
2019-03-09 07:35:38 +0900

    assets precompile off

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,5 +95,4 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   config.action_mailer.delivery_method = :letter_opener_web
 
-  config.assets.initialize_on_precompile = false
 end
