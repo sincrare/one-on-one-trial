b259c4ff9e67352815694b010fb1b320da0c79af
門田啓吾 <sincrare@sonicgarden.jp>
2019-03-10 06:55:53 +0900

    change production host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,6 +93,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'one-on-one-trial.herokuapp.com', port: 443 }
+  config.action_mailer.default_url_options = { host: 'one-on-one-trial.herokuapp.com' }
   config.action_mailer.delivery_method = :letter_opener_web
 end
